21f4240017d4f46990b9bc9316977da0bc291a15
Ian Clyde Tejada <ianclyde.tejada-16@cpu.edu.ph>
2024-02-11 23:25:15 +0800

    fix 4

--- db/firebaseAdmin.ts
+++ db/firebaseAdmin.ts
@@ -1,47 +1,19 @@
 import "server-only";
 import admin from "firebase-admin";
 
-interface FirebaseAdminAppParams {
-  projectId: string;
-  clientEmail: string;
-  storageBucket: string;
-  privateKey: string;
-}
+import serviceAccount from "../visionx-413000-firebase-adminsdk-aus09-8606f823c1.json";
 
-function formatPrivateKey(key: string) {
-  return key.replace(/\\n/g, "\n");
-}
-
-export function createFirebaseAdminApp(params: FirebaseAdminAppParams) {
-  const privateKey = params.privateKey
-    ? formatPrivateKey(params.privateKey)
-    : undefined;
-
+export function createFirebaseAdminApp() {
   if (admin.apps.length > 0) {
     return admin.app();
   }
 
-  const cert = admin.credential.cert({
-    projectId: params.projectId,
-    clientEmail: params.clientEmail,
-    privateKey,
-  });
-
   return admin.initializeApp({
-    credential: cert,
-    projectId: params.projectId,
-    storageBucket: params.storageBucket,
+    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   });
 }
 
 export async function initAdmin() {
-  const params = {
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID as string,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL as string,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET as string,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY as string,
-  };
-
-  return createFirebaseAdminApp(params);
+  return createFirebaseAdminApp();
 }
-
