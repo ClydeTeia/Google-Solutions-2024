b944146ccc2957d4923020f22d91378441abefd5
chdean-09 <chaddenard.andrada-19@cpu.edu.ph>
2024-02-13 23:21:29 +0800

    start over firebase config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,19 @@
 import Image from "next/image";
-import { initAdmin } from "../../db/firebaseAdmin";
-import { getLinks, getLogoFromStorage } from "../../db/firebase";
 
 export default async function Home() {
-  await initAdmin();
-  const links = await getLinks();
-  const logo = await getLogoFromStorage();
 
   return (
     <main className="p-2">
       <h1>Home Page</h1>
-      <Image src={logo} alt={"logo"} width={100} height={24} priority></Image>
+      {/* <Image src={logo} alt={"logo"} width={100} height={24} priority></Image> */}
       <div></div>
       <div>
-        {links?.map((link) => (
+        {/* {links?.map((link) => (
           <a href={link.url} key={link.title}>
             <h2 className="text-3xl">{link.title}</h2>
             <p>{link.desc}</p>
           </a>
-        ))}
+        ))} */}
       </div>
     </main>
   );
